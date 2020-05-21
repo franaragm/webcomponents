7a850ddebdec93a56e57c6f93f28f72a64a5c309
Fran Aragon <faragon@vectoritcgroup.com>
2020-05-21 18:20:19 +0200

    initial commit

---
+++ src/components/00-spash-screen/webcomponent-project.js
@@ -0,0 +1,3 @@
+import { WebcomponentProject } from './WebcomponentProject.js';
+
+customElements.define('webcomponent-project', WebcomponentProject);
---
+++ stories/webcomponent-project.stories.js
@@ -0,0 +1,11 @@
+import { html } from 'lit-html';
+import '../src/webcomponent-project.js';
+
+export default {
+  title: 'webcomponent-project',
+};
+
+export const App = () =>
+  html`
+    <webcomponent-project></webcomponent-project>
+  `;
